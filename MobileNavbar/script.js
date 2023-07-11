2b2c6f317906b9ce2f8abde08960c62b9afe0117
Akshay_linux <piratekiller25@gmail.com>
2023-07-11 17:51:57 +0530

    initial navbar

---
+++ MobileNavbar/script.js
@@ -0,0 +1,11 @@
+// const btnClick=document.querySelector(".btn");
+// const showMenu=document.querySelector("ul");
+// btnClick.addEventListener("click", myFunction=()=>{
+//     if(showMenu.style.display==='none'){
+//         showMenu.style.display='block'; 
+//     }else{
+//         showMenu.style.display='none';
+//     }
+// });
+
+// ---------------------------------------------------//
